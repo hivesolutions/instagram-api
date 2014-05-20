502e1ab4025e571d209d7c624831f1b77dace61a
joamag <joamag@gmail.com>
2014-05-20 13:59:35 +0100

    small base code refactor

--- src/examples/base.py
+++ src/examples/base.py
@@ -39,11 +39,11 @@ __license__ = "GNU General Public License (GPL), Version 3"
 
 import appier
 
-import facebook
+import instagram
 
 def get_api():
-    return facebook.Api(
-        client_id = appier.conf("FB_ID"),
-        client_secret = appier.conf("FB_SECRET"),
-        redirect_url = appier.conf("FB_REDIRECT_URL")
+    return instagram.Api(
+        client_id = appier.conf("INSTAGRAM_ID"),
+        client_secret = appier.conf("INSTAGRAM_SECRET"),
+        redirect_url = appier.conf("INSTAGRAM_REDIRECT_URL")
     )

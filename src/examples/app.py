154902159405848ff44104a6508dd9831c0edeeb
joamag <joamag@gmail.com>
2014-05-20 13:58:46 +0100

    new examples for api

---
+++ src/examples/app.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Instagram API
+# Copyright (C) 2008-2014 Hive Solutions Lda.
+#
+# This file is part of Hive Instagram API.
+#
+# Hive Instagram API is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Instagram API is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Instagram API. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2014 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import appier
+
+from examples import base
+
+class InstagramApp(appier.WebApp):
+
+    def __init__(self):
+        appier.WebApp.__init__(self, name = "instagram")
+
+    @appier.route("/", "GET")
+    def index(self):
+        return self.me()
+
+    @appier.route("/me", "GET")
+    def me(self):
+        url = self.ensure_api()
+        if url: return self.redirect(url)
+        api = self.get_api()
+        user = api.self_user()
+        return user
+
+    @appier.route("/oauth", "GET")
+    def oauth(self):
+        code = self.field("code")
+        api = self.get_api()
+        access_token = api.oauth_access(code)
+        self.session["ig.access_token"] = access_token
+        return self.redirect(
+            self.url_for("instagram.index")
+        )
+
+    @appier.exception_handler(appier.OAuthAccessError)
+    def oauth_error(self, error):
+        if "ig.access_token" in self.session: del self.session["ig.access_token"]
+        return self.redirect(
+            self.url_for("instagram.index")
+        )
+
+    def ensure_api(self):
+        access_token = self.session.get("ig.access_token", None)
+        if access_token: return
+        api = base.get_api()
+        return api.oauth_authorize()
+
+    def get_api(self):
+        access_token = self.session and self.session.get("ig.access_token", None)
+        api = base.get_api()
+        api.access_token = access_token
+        return api
+
+if __name__ == "__main__":
+    app = InstagramApp()
+    app.serve()

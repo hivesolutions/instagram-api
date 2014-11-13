934a671cf7047e66c83bf7c47d730aa9674de574
joamag <joamag@gmail.com>
2014-11-13 13:08:50 +0000

    new version

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ import setuptools
 
 setuptools.setup(
     name = "instagram_api",
-    version = "0.1.0",
+    version = "0.1.1",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Instagram API",
